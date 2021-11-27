58fcaa77ad6a590824387bc12a84228b478e6fcb
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-27 17:05:38 +0530

    Update manage.py

--- ChannelBot/manage.py
+++ ChannelBot/manage.py
@@ -25,6 +25,6 @@ async def manage_channels(user_id, bot: Client):
         return True, buttons, text
     else:
         buttons = [
-                ['+ Add Channels +']
+                ['📢 Add channels']
         ]
         return False, buttons, 'No Channels Found. Add a channel using below button'
