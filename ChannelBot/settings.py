5fd2dd00a1e8745b7115025827dba609004c1845
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-27 17:25:51 +0530

    Update settings.py

--- ChannelBot/settings.py
+++ ChannelBot/settings.py
@@ -47,8 +47,8 @@ async def channel_settings(channel_id, bot):
                 InlineKeyboardButton(f'{emoji.KEYBOARD}Buttons', callback_data=f'change+buttons+{channel_id}')
             ],
             [InlineKeyboardButton(f'{emoji.UPDATE}Caption Mode : {position.capitalize()}', callback_data=f'change+position+{channel_id}+{position}')],
-            [InlineKeyboardButton(f'{emoji.HOMES}Sticker', callback_data=f'change+sticker+{channel_id}')],
-            [InlineKeyboardButton(f'{emoji.PEN}Edit Mode : {edit_mode.capitalize()}', callback_data=f'change+edit_mode+{channel_id}+{edit_mode}')],
+            [InlineKeyboardButton(f'{emoji.HOMES}Sticker', callback_data=f'change+sticker+{channel_id}'),
+             InlineKeyboardButton(f'{emoji.PEN}Edit Mode : {edit_mode.capitalize()}', callback_data=f'change+edit_mode+{channel_id}+{edit_mode}')],
             [InlineKeyboardButton(f'{emoji.OPEN_BOOK}Webpage Preview : {webpage_preview}', callback_data=f'change+webpage_preview+{channel_id}+{webpage_preview}')],
             [InlineKeyboardButton(f'{emoji.WASTEBASKET}Remove Channel', callback_data=f'remove+{channel_id}')],
             [InlineKeyboardButton('🔙 Back', callback_data='home+channels')]
